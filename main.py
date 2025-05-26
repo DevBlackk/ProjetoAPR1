be9207ffc78a42300e522202cb26433bfad5174b
Davidson William <davidsonwferreira8@gmail.com>
2025-05-26 15:16:04 -0300

    Criação do menu de interaçôes do projeto.

---
+++ main.py
@@ -0,0 +1,20 @@
+def menu_principal():
+    print("1. Usúario")
+    print("2. Livros")
+    print("3. Empréstimos")
+    print("4. Relatórios")
+    print("5. Sair")
+    opcao = int(input("\nEscolha uma opção: "))
+
+    if opcao == 1:
+        print("Este serviço não diponivel!")
+    elif opcao == 2:
+        print("Este serviço não diponivel!")
+    elif opcao == 3:
+        print("Este serviço não diponivel!")
+    elif opcao == 4:
+        print("Este serviço não diponivel!")
+    else:
+        print("Enserrando o programa!")
+
+menu_principal()
