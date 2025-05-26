25253297c88418654fc1b9c6d8c282dbe619619a
Davidson William <davidsonwferreira8@gmail.com>
2025-05-26 16:13:32 -0300

    Criado a interação do menu usúario.

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 def menu_principal():
-    print("1. Usúarios")
-    print("2. Livros")
-    print("3. Empréstimos")
-    print("4. Relatórios")
-    print("5. Sair")
-    opcao = int(input("\nEscolha uma opção: "))
-
-    if opcao == 1:
-        menu_usuario()
-    elif opcao == 2:
-        print("Este serviço não diponivel!")
-    elif opcao == 3:
-        print("Este serviço não diponivel!")
-    elif opcao == 4:
-        print("Este serviço não diponivel!")
-    else:
-        print("Enserrando o programa!")
+    executando = True
+    while executando:
+        print("\nMenu Principal:")
+        print("1. Usúarios")
+        print("2. Livros")
+        print("3. Empréstimos")
+        print("4. Relatórios")
+        print("5. Sair")
+        
+        opcao = int(input("\nEscolha uma opção: "))
+        if opcao == 1:
+            if not menu_usuario():
+                executando = False
+        elif opcao == 2:
+            print("Este serviço não esta disponivel!")
+        elif opcao == 3:
+            print("Este serviço não esta disponivel!")
+        elif opcao == 4:
+            print("Este serviço não esta disponivel!")
+        elif opcao == 5:
+            print("Encerrando o programa!")
+            executando = False
+        else:
+            print("Opção inválida! Por favor, digite uma das opções acima.")
 
 def menu_usuario():
-    print("\nMenu de usúarios:")
-    print("1. Ver lista de usúarios")
-    print("2. Editar usúarios")
-    print("3. Criar um novo usúario")
-    print("4. Deletar um usúario")
-    print("5. Voltar")
-    opcao = int(input("\nEscolha uma opção: "))
+    sem_menu_usuario = True
+    while sem_menu_usuario:
+        print("\nMenu de usúarios:")
+        print("1. Ver lista de usúarios")
+        print("2. Editar usúarios")
+        print("3. Criar um novo usúario")
+        print("4. Deletar um usúario")
+        print("5. Voltar")
+        print("6. Sair do Programa")
+        
+        opcao = int(input("\nEscolha uma opção: "))
 
-    if opcao == 1:
-        print("Este serviço não diponivel!\n")
-        menu_principal()
-    elif opcao == 2:
-        print("Este serviço não diponivel!\n")
-        menu_principal()
-    elif opcao == 3:
-        print("Este serviço não diponivel!\n")
-        menu_principal()
-    elif opcao == 4:
-        print("Este serviço não diponivel!\n")
-        menu_principal()
-    elif opcao == 5:
-        menu_principal()
-    else:
-        print("Opção invalida! Por favor, digite uma das opções acima.\n")
-        menu_usuario()
+        if opcao == 1:
+            print("Este serviço não diponivel!\n")
+        elif opcao == 2:
+            print("Este serviço não diponivel!\n")
+        elif opcao == 3:
+            print("Este serviço não diponivel!\n")
+        elif opcao == 4:
+            print("Este serviço não diponivel!\n")
+        elif opcao == 5:
+            return True
+        elif opcao == 6:
+            return False
+        else:
+            print("Opção invalida! Por favor, digite uma das opções acima.")
 
 def main():
     menu_principal()
-
 main()
