61507364dcb6a6f684cce40b21bdefbbdf0089b0
Davidson William <davidsonwferreira8@gmail.com>
2025-05-29 00:16:20 -0300

    Criado a função de excuir usuário.

--- main.py
+++ main.py
@@ -1,237 +1,195 @@
+usuarios = []
+
+def carregar_usuarios():
+    """Carrega usuários do arquivo (implementar depois)"""
+    pass
+
+def salvar_usuarios():
+    """Salva usuários no arquivo (implementar depois)"""
+    pass
+
+def criar_usuario():
+    try:
+        usuario = {
+            "nome": input("Digite o nome: "),
+            "cpf": input("Digite o CPF: "),
+            "endereco": input("Digite o endereço: "),
+            "numero": int(input("Digite o número: ")),
+            "cep": input("Digite o CEP: "),
+            "emails": [],
+            "telefones": [],
+            "data_nasc": input("Digite a data de nascimento: "),
+            "profissao": input("Digite a profissão: ")
+        }
+
+        while True:
+            email = input("Digite um email (ou 'sair' para terminar): ")
+            if email.lower() == 'sair':
+                break
+            usuario["emails"].append(email)
+
+        while True:
+            telefone = input("Digite um telefone (ou 'sair' para terminar): ")
+            if telefone.lower() == 'sair':
+                break
+            usuario["telefones"].append(telefone)
+
+        usuarios.append(usuario)
+        print(f"Usuário {usuario['nome']} adicionado com sucesso!")
+        return True
+
+    except ValueError:
+        print("Erro: por favor, insira um número válido para o campo 'numero'.")
+        return False
+    except Exception as e:
+        print(f"Erro ao criar usuário: {str(e)}")
+        return False
+
+def excluir_usuario():
+    if not usuarios:
+        print("Nenhum usuário cadastrado!")
+        return False
+    
+    try:
+        cpf = input("Digite o CPF do usuário que deseja excluir: ")
+        usuario = buscar_usuario_por_cpf(cpf)
+        
+        if usuario:
+            print("\nDados do usuário a ser excluído:")
+            mostrar_usuario(usuario)
+            
+            confirmacao = input("\nTem certeza que deseja excluir este usuário? (s/n): ")
+            if confirmacao.lower() == 's':
+                usuarios.remove(usuario)
+                print(f"\nUsuário {usuario['nome']} excluído com sucesso!")
+                return True
+            else:
+                print("\nOperação cancelada!")
+                return False
+        else:
+            print("Usuário não encontrado!")
+            return False
+            
+    except Exception as e:
+        print(f"Erro ao excluir usuário: {str(e)}")
+        return False
+
+def listar_usuarios():
+    while True:
+        print("\nListar Usuários:")
+        print("1. Listar todos os usuários")
+        print("2. Buscar usuário específico")
+        print("3. Voltar")
+        
+        try:
+            opcao = int(input("\nEscolha uma opção: "))
+            
+            if opcao == 1:
+                if not usuarios:
+                    print("Nenhum usuário cadastrado!")
+                    return
+                
+                for i, usuario in enumerate(usuarios, 1):
+                    print(f"\nUsuário {i}:")
+                    mostrar_usuario(usuario)
+                    
+            elif opcao == 2:
+                if not usuarios:
+                    print("Nenhum usuário cadastrado!")
+                    return
+                
+                cpf = input("Digite o CPF do usuário: ")
+                usuario = buscar_usuario_por_cpf(cpf)
+                if usuario:
+                    mostrar_usuario(usuario)
+                else:
+                    print("Usuário não encontrado!")
+                    
+            elif opcao == 3:
+                return
+            else:
+                print("Opção inválida!")
+                
+        except ValueError:
+            print("Por favor, digite um número válido!")
+
+def mostrar_usuario(usuario):
+    print(f"Nome: {usuario['nome']}")
+    print(f"CPF: {usuario['cpf']}")
+    print(f"Endereço: {usuario['endereco']}, {usuario['numero']}")
+    print(f"CEP: {usuario['cep']}")
+    print(f"Data de Nascimento: {usuario['data_nasc']}")
+    print(f"Profissão: {usuario['profissao']}")
+    print("Emails:", ", ".join(usuario['emails']))
+    print("Telefones:", ", ".join(usuario['telefones']))
+    print("-" * 50)
+
+def buscar_usuario_por_cpf(cpf):
+    for usuario in usuarios:
+        if usuario['cpf'] == cpf:
+            return usuario
+    return None
+
+def menu_usuario():
+    while True:
+        print("\nMenu de Usuários:")
+        print("1. Ver lista de usuários")
+        print("2. Editar usuários")
+        print("3. Criar um novo usuário")
+        print("4. Deletar um usuário")
+        print("5. Voltar")
+        print("6. Sair do Programa")
+        
+        try:
+            opcao = int(input("\nEscolha uma opção: "))
+
+            if opcao == 1:
+                listar_usuarios()
+            elif opcao == 2:
+                print("Este serviço não está disponível!\n")
+            elif opcao == 3:
+                criar_usuario()
+            elif opcao == 4:
+                excluir_usuario()
+            elif opcao == 5:
+                return True
+            elif opcao == 6:
+                return False
+            else:
+                print("Opção inválida!")
+        except ValueError:
+            print("Por favor, digite um número válido!")
+
 def menu_principal():
-    executando = True
-    while executando:
+    while True:
         print("\nMenu Principal:")
-        print("1. Usúarios")
+        print("1. Usuários")
         print("2. Livros")
         print("3. Empréstimos")
         print("4. Relatórios")
         print("5. Sair")
         
-        opcao = int(input("\nEscolha uma opção: "))
-        if opcao == 1:
-            if not menu_usuario():
-                executando = False
-        elif opcao == 2:
-            print("Este serviço não esta disponivel!")
-        elif opcao == 3:
-            print("Este serviço não esta disponivel!")
-        elif opcao == 4:
-            print("Este serviço não esta disponivel!")
-        elif opcao == 5:
-            print("Encerrando o programa!")
-            executando = False
-        else:
-            print("Opção inválida! Por favor, digite uma das opções acima.")
-
-def criar_lista(dados, menssagem):
-	lista_usuario = []
-	lista_usuario.append(dados)
-	print(f"{menssagem} adicionado com sucesso!")
-	return lista_usuario
-
-def criar_usuario():
-    criando = True
-	while criando:
-		try:
-			usuario = {
-			"nome": input("Digite o nome: "),
-			"cpf": input("Digite o CPF: "),
-			"endereco": input("Digite o endereço: "),
-			"numero": int(input("Digite o número: ")),
-			"cep": input("Digite o CEP: "),
-			"emails": [],
-			"telefones": [],
-			"data_nasc": input("Digite a data de nascimento: "),
-			"profissao": input("Digite a profissão: ")
-			}
-
-			multiplos_email = True
-			while multiplos_email:
-				email = input("Digite um email (ou 'sair' para terminar): ")
-				if email.lower() == 'sair':
-					multiplos_email = False
-				usuario["emails"].append(email)
-
-			multiplos_telfones = True
-			while multiplos_telfones:
-				telefone = input("Digite um telefone (ou 'sair' para terminar): ")
-				if telefone.lower() == 'sair':
-					multiplos_telfones = False
-				usuario["telefones"].append(telefone)
-
-			return criar_lista(usuario, f"{usuario['nome']}")
-
-		except ValueError:
-			print("Erro: por favor, insira um numero válido para o campo 'numero'.")
-		except Exception as e:
-			print(f"Erro ao criar usuário: {str(e)}")
-
-def menu_usuario():
-    sem_menu_usuario = True
-    while sem_menu_usuario:
-        print("\nMenu de usúarios:")
-        print("1. Ver lista de usúarios")
-        print("2. Editar usúarios")
-        print("3. Criar um novo usúario")
-        print("4. Deletar um usúario")
-        print("5. Voltar")
-        print("6. Sair do Programa")
-        
-        opcao = int(input("\nEscolha uma opção: "))
-
-        if opcao == 1:
-            print("Este serviço não diponivel!\n")
-        elif opcao == 2:
-            print("Este serviço não diponivel!\n")
-        elif opcao == 3:
-            lista_usuarios = criar_usuario()
-            if lista_usuarios:
-                print("Usuário criado com sucesso!")
+        try:
+            opcao = int(input("\nEscolha uma opção: "))
+            
+            if opcao == 1:
+                if not menu_usuario():
+                    break
+            elif opcao == 2:
+                print("Este serviço não está disponível!")
+            elif opcao == 3:
+                print("Este serviço não está disponível!")
+            elif opcao == 4:
+                print("Este serviço não está disponível!")
+            elif opcao == 5:
+                print("Encerrando o programa!")
+                break
             else:
-                print("Erro ao criar usuário!")
-        elif opcao == 4:
-            print("Este serviço não diponivel!\n")
-        elif opcao == 5:
-            return True
-        elif opcao == 6:
-            return False
-        else:
-            print("Opção invalida! Por favor, digite uma das opções acima.")
+                print("Opção inválida!")
+        except ValueError:
+            print("Por favor, digite um número válido!")
 
-
-
-def main():
+if __name__ == "__main__":
+    carregar_usuarios()
     menu_principal()
-    criar_usúario()
-main()
-
-'''
-[
-	{
-		"nome": "Iago Alexandre Samuel Moraes",
-		"idade": 47,
-		"cpf": "368.715.054-23",
-		"rg": "48.041.981-4",
-		"data_nasc": "07/04/1978",
-		"sexo": "Masculino",
-		"signo": "Áries",
-		"mae": "Alícia Bruna",
-		"pai": "Davi Edson Moraes",
-		"email": "iago-moraes96@c-a-m.com",
-		"senha": "fw65bAtpkw",
-		"cep": "50690-715",
-		"endereco": "Rua Formosa do Araguaia",
-		"numero": 390,
-		"bairro": "Iputinga",
-		"cidade": "Recife",
-		"estado": "PE",
-		"telefone_fixo": "(81) 2982-7453",
-		"celular": "(81) 98733-6681",
-		"altura": "1,66",
-		"peso": 60,
-		"tipo_sanguineo": "B-",
-		"cor": "amarelo"
-	},
-	{
-		"nome": "Sérgio Pedro José Moreira",
-		"idade": 49,
-		"cpf": "189.754.211-98",
-		"rg": "40.831.329-8",
-		"data_nasc": "10/01/1976",
-		"sexo": "Masculino",
-		"signo": "Capricórnio",
-		"mae": "Stefany Alessandra",
-		"pai": "Raimundo Gabriel Moreira",
-		"email": "sergiopedromoreira@pubdesign.com.br",
-		"senha": "u1wXE0lk3s",
-		"cep": "29075-665",
-		"endereco": "Rua Doutora Zilda Arns",
-		"numero": 359,
-		"bairro": "Aeroporto",
-		"cidade": "Vitória",
-		"estado": "ES",
-		"telefone_fixo": "(27) 2966-8693",
-		"celular": "(27) 99917-8385",
-		"altura": "1,70",
-		"peso": 75,
-		"tipo_sanguineo": "B+",
-		"cor": "laranja"
-	},
-	{
-		"nome": "Miguel Iago Drumond",
-		"idade": 24,
-		"cpf": "744.704.420-20",
-		"rg": "24.612.924-4",
-		"data_nasc": "12/04/2001",
-		"sexo": "Masculino",
-		"signo": "Áries",
-		"mae": "Alessandra Marina",
-		"pai": "André Yuri Drumond",
-		"email": "miguel-drumond85@grupomegavale.com.br",
-		"senha": "dywkGQymhu",
-		"cep": "76962-208",
-		"endereco": "Avenida Itapemirim",
-		"numero": 913,
-		"bairro": "Novo Cacoal",
-		"cidade": "Cacoal",
-		"estado": "RO",
-		"telefone_fixo": "(69) 3999-9948",
-		"celular": "(69) 99528-6924",
-		"altura": "1,68",
-		"peso": 81,
-		"tipo_sanguineo": "B+",
-		"cor": "vermelho"
-	},
-	{
-		"nome": "Ian Felipe da Costa",
-		"idade": 66,
-		"cpf": "038.718.631-06",
-		"rg": "37.517.985-9",
-		"data_nasc": "08/04/1959",
-		"sexo": "Masculino",
-		"signo": "Áries",
-		"mae": "Milena Antônia",
-		"pai": "Matheus Mário da Costa",
-		"email": "ian.felipe.dacosta@compuativa.com.br",
-		"senha": "oPtN7qbOnh",
-		"cep": "57061-624",
-		"endereco": "Rua Gastone Lúcia de Carvalho Beltrão",
-		"numero": 767,
-		"bairro": "Tabuleiro do Martins",
-		"cidade": "Maceió",
-		"estado": "AL",
-		"telefone_fixo": "(82) 3598-3617",
-		"celular": "(82) 99258-0843",
-		"altura": "1,65",
-		"peso": 55,
-		"tipo_sanguineo": "A-",
-		"cor": "amarelo"
-	},
-	{
-		"nome": "Marcelo Joaquim Antonio Silveira",
-		"idade": 79,
-		"cpf": "802.002.117-57",
-		"rg": "40.322.127-4",
-		"data_nasc": "19/03/1946",
-		"sexo": "Masculino",
-		"signo": "Peixes",
-		"mae": "Emilly Isis Jennifer",
-		"pai": "Theo Kevin Silveira",
-		"email": "marcelo-silveira74@findout.com.br",
-		"senha": "B3UqJfbHlR",
-		"cep": "07400-190",
-		"endereco": "Rua Angelina Augusto Fernandes",
-		"numero": 216,
-		"bairro": "Centro",
-		"cidade": "Arujá",
-		"estado": "SP",
-		"telefone_fixo": "(11) 3644-7317",
-		"celular": "(11) 99476-2918",
-		"altura": "1,62",
-		"peso": 75,
-		"tipo_sanguineo": "B-",
-		"cor": "vermelho"
-	}
-]
-'''
+    salvar_usuarios()
